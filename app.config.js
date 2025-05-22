4ad68183eae629a7dda06a3b01e60f923daa8406
Dineshkumar Mohandoss <dineshkumar.mohandoss@tekafforde.com>
2025-05-22 10:32:51 +0530

    app config changes

--- app.config.js
+++ app.config.js
@@ -1,36 +1,35 @@
 export default {
-  name: 'StormCast',
-  slug: 'stormcast',
-  version: '1.0.0',
-  orientation: 'portrait',
-  icon: './assets/icon.png',
-  userInterfaceStyle: 'light',
-  scheme: 'stormcast',
+  name: "StormCast",
+  slug: "stormcast",
+  owner: "dinesh-exe",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  scheme: "stormcast",
   splash: {
-    image: './assets/splash.png',
-    resizeMode: 'contain',
-    backgroundColor: '#ffffff'
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
   },
-  assetBundlePatterns: [
-    '**/*'
-  ],
+  assetBundlePatterns: ["**/*"],
   ios: {
     supportsTablet: true,
-    bundleIdentifier: 'com.stormcast.app'
+    bundleIdentifier: "com.stormcast.app",
   },
   android: {
     adaptiveIcon: {
-      foregroundImage: './assets/adaptive-icon.png',
-      backgroundColor: '#ffffff'
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
     },
-    package: 'com.stormcast.app'
+    package: "com.stormcast.app",
   },
   web: {
-    favicon: './assets/favicon.png'
+    favicon: "./assets/favicon.png",
   },
   extra: {
     eas: {
-      projectId: 'your-project-id'
-    }
-  }
-}; 
+      projectId: "345319a6-5493-4758-ba25-1a6b155cf6e9",
+    },
+  },
+};
